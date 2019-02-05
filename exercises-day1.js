ffa6a35b138caf23c275f016c003c28574000665
MuhammadNd88 <46718678+MuhammadNd88@users.noreply.github.com>
2019-02-05 07:19:51 -0700

    Final Assignment

---
+++ exercises-day1.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// Create an index.html page and add an external link to this .js file.
+document.write("Successfully Linked Exercises Day 1");
+
+// Open up index.html in the browser. If you see "Successfully Linked Exercises Day 1" on your page, you may continue.
+
+//1. Define a new variable `quote` for the value "A ship in port is safe, but that is not what ships are for. Sail out to sea and do new things."
+
+var quote =
+  "A ship in port is safe, but that is not what ships are for. sail out to sea and do new things.";
+//Log out the quote
+
+console.log(quote);
+
+//Log out the type of the variable. Is it a string? A number? Something else?
+console.log(typeof quote);
+//2. Define a variable `quoteLength` that stores the length of the quote.
+var qoutelength = quote.length;
+//Log out the length
+console.log(qoutelength);
+//3. Create a new variable `attributedQuote`. Add the string "- Rear Admiral Grace Hopper" to the end of the `quote` variable with an appropriate space in between.
+//This new value should not _replace_ the `quote` variable's value
+//Log out the new quote
+var attributedQoute = "- Rear Admiral Grace Hopper";
+var qoute = console.log(quote + " " + attributedQoute);
+
+//4. Create a function `attribute` that takes a quote and author and returns the attributed quote.
+
+function attribute(qoute, auther) {
+  return qoute + " " + auther;
+}
+var qoute1 =
+  "The key to being a successful programmer is to learn how to learn.";
+var author1 = "- Shannon Burns";
+console.log(qoute1, author1);
+
+//Example: Takes "The key to being a successful programmer is to learn how to learn." and "- Shannon Burns" and returns "The key to being a successful programmer is to learn how to learn. - Shannon Burns"
+
+//5. Create a function `sumOfNumbers` that takes 2 numbers and returns the sum of those numbers.
+
+function sumNumber(x, y) {
+  return x + y;
+}
+
+var a = 1;
+var b = 1;
+
+console.log(sumNumber(a, b));
+
+//6. Create a function `sumOfLowNumbers` that takes 5 numbers and returns the sum of all numbers that are less than or equal to 6.
+function sumFiveNumber(x, y, z, p, q) {
+  return x + y + z + p + q;
+
+  if (a <= 6 && b <= 6) {
+    if (c <= 6 && d <= 6) {
+      if (e <= 6) {
+        return a + b + c + d + e;
+      }
+      console.log("wrong Numbers");
+    }
+    console.log("wrong Numbers");
+  } else {
+    console.log("wrong Numbers");
+  }
+}
+console.log(sumFiveNumber(2, 2, 3, 4, 5));
+//7. Go to https://pages.github.com/ and read through the tutorial to create a Github page.
+// Choose Project site, Start from scratch. When you're finished, link your new project page here.
+
+//8. Commit and push this file to your repository. Submit the URL for Assignment 1 on Brightspace.
